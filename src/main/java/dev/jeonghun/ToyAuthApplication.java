a6347bd99196c414165889199714bb71aa492562
jeonghuntech <jeonghun.tech@gmail.com>
2024-01-03 15:05:50 +0900

    first commit

---
+++ src/main/java/dev/jeonghun/ToyAuthApplication.java
@@ -0,0 +1,13 @@
+package dev.jeonghun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyAuthApplication.class, args);
+	}
+
+}
