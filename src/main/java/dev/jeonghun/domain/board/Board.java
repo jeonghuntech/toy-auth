c2b4bc5cfa750bde6bbd13838abdf79917eb56ca
jeonghuntech <jeonghun.tech@gmail.com>
2024-01-18 14:48:26 +0900

    feat: 게시판 엔티티 생성

---
+++ src/main/java/dev/jeonghun/domain/board/Board.java
@@ -0,0 +1,31 @@
+package dev.jeonghun.domain.board;
+
+import dev.jeonghun.domain.DeleteFlag;
+import dev.jeonghun.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 1, nullable = false)
+    @ColumnDefault("'N'")
+    @Enumerated(EnumType.STRING)
+    private DeleteFlag deleted = DeleteFlag.N;
+}
