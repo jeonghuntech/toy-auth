58525b10ed46d9937e01f4e1219a1688881b2153
jeonghuntech <jeonghun.tech@gmail.com>
2024-01-21 15:26:14 +0900

    refactor: @Repository 어노테이션 누락 추가

--- src/main/java/dev/jeonghun/repository/CommentRepository.java
+++ src/main/java/dev/jeonghun/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package dev.jeonghun.repository;
 
 import dev.jeonghun.domain.board.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/dev/jeonghun/repository/MemberRepository.java
+++ src/main/java/dev/jeonghun/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package dev.jeonghun.repository;
 import dev.jeonghun.domain.DeleteFlag;
 import dev.jeonghun.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByIdAndDeleted(long userId, DeleteFlag deleted);
 }
