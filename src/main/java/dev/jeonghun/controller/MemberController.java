b76292d8ee4ef4c1856e8492d7e52fc998b920ff
jeonghuntech <jeonghun.tech@gmail.com>
2024-01-25 21:24:49 +0900

    feat: 페이징 멤버 전체 조회 api 추가

--- src/main/java/dev/jeonghun/controller/MemberController.java
+++ src/main/java/dev/jeonghun/controller/MemberController.java
@@ -1,24 +1,27 @@
 package dev.jeonghun.controller;
 
-import dev.jeonghun.domain.dto.MemberDto;
-import dev.jeonghun.service.MemberService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.jeonghun.domain.dto.MemberDto;
+import dev.jeonghun.service.MemberService;
+
 @RestController
+@RequestMapping("/member")
 public class MemberController {
 
-    private final MemberService memberService;
+	private final MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
 
-    @GetMapping(value = "/members")
-    public ResponseEntity<Page<MemberDto>> findAllMembers(Pageable pageable) {
-        return ResponseEntity.ok(memberService.findAllMembers(pageable));
-    }
+	@GetMapping("/list")
+	public ResponseEntity<Page<MemberDto>> findAllMembers(Pageable pageable) {
+		return ResponseEntity.ok(memberService.findAllMembers(pageable));
+	}
 }
--- src/main/java/dev/jeonghun/service/MemberService.java
+++ src/main/java/dev/jeonghun/service/MemberService.java
@@ -1,24 +1,36 @@
 package dev.jeonghun.service;
 
-import dev.jeonghun.domain.dto.MemberDto;
-import dev.jeonghun.repository.MemberRepository;
-import jakarta.transaction.Transactional;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import dev.jeonghun.domain.Member;
+import dev.jeonghun.domain.dto.MemberDto;
+import dev.jeonghun.repository.MemberRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
-    public Page<MemberDto> findAllMembers(Pageable pageable) {
-        return memberRepository.findAllBy(pageable)
-                .map(MemberDto::new);
-    }
+	public Page<MemberDto> findAllMembers(Pageable pageable) {
+		return memberRepository.findAllBy(pageable)
+			.map(MemberDto::new);
+	}
+
+	public void save(Member member) {
+		memberRepository.save(member);
+	}
+
+	public void saveAll(List<Member> members) {
+		memberRepository.saveAll(members);
+	}
 }
